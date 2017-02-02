6f61be52930a365c9b9d99ba8a7d2dafd823f45e
=^._.^= <maxogden@users.noreply.github.com>
2017-02-02 09:02:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var fs = require('fs')
 var debug = require('debug')('toiletdb')
 
 module.exports = function (filename) {
+  // in memory copy of latest state that functions below mutate
   var state = {}
 
+  // `low` ensures if write is called multiple times at once the last one will be executed
+  // last and call the callback. this works OK because we have `state` above
   var write = low(function (writeState, cb) {
-    var payload = JSON.stringify(writeState, null, '  ')
+    var payload = JSON.stringify(writeState, null, '  ') // pretty printed
     debug('writing', filename, payload)
+    
+    // write to tempfile first so we know it fully writes to disk and doesnt corrupt existing file
     var tmpname = filename + '.' + Math.random()
     fs.writeFile(tmpname, payload, function (err) {
       if (err) {
@@ -24,24 +29,28 @@ module.exports = function (filename) {
       fs.readFile(filename, function (err, buf) {
         if (err) {
           if (err.code === 'ENOENT') {
+            // if you read before ever writing
             return cb(null, state)
           } else {
             return cb(err)
           }
         }
         try {
+          // if youre using toiletdb your db needs to fit in a single string
           var jsonString = buf.toString()
-          state = JSON.parse(jsonString)
+          var parsed = JSON.parse(jsonString)
           debug('reading', filename, jsonString)
+          return cb(null, parsed)
         } catch (e) {
           return cb(e)
         }
-        cb(null, state)
       })
     },
     write: function (key, data, cb) {
+      // json doesnt support binary
       if (Buffer.isBuffer(key)) key = key.toString('hex')
       if (Buffer.isBuffer(data)) data = data.toString('hex')
+      // the '|| null' is because JSON.stringify deletes keys with `undefined` values
       state[key] = data || null
       write(state, cb)
     },
